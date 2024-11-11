305280c4ac9e97bca28c474347b1501921c26cb6
Igor Lucca <83629029+igorlucca11@users.noreply.github.com>
2024-11-11 19:12:29 -0300

    Update artilharia.js

--- backend/Gols/artilharia.js
+++ backend/Gols/artilharia.js
@@ -2,7 +2,7 @@ const pool = require('../db');
 
 const getArtilharia = (req, res) => {
   const query = `
-    SELECT jogadores.nome_completo AS jogador, times.nome AS time, COUNT(gols.id) AS gols
+    SELECT DISTINCT jogadores.nome_completo AS jogador, times.nome AS time, COUNT(gols.id) AS gols
     FROM gols
     JOIN jogadores ON gols.jogador_id = jogadores.id
     JOIN times ON jogadores.time_id = times.id
