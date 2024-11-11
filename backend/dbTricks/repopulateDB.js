680944b9de3f93287f972a37dfcf7419dde2c556
Igor Lucca <igor.lucca.mp4@gmail.com>
2024-11-11 00:29:21 -0300

    repopulate db trick

--- backend/dbTricks/repopulateDB.js
+++ backend/dbTricks/repopulateDB.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
 });
 
 const createTables = () => {
@@ -74,108 +74,181 @@ const createTables = () => {
 
   pool.query(createUsuariosTable, (err, results) => {
     if (err) {
-      console.error('Erro ao criar a tabela de usuários:', err);
+      console.error("Erro ao criar a tabela de usuários:", err);
     } else {
-      console.log('Tabela de usuários criada ou já existe.');
+      console.log("Tabela de usuários criada ou já existe.");
     }
   });
 
   pool.query(createTimesTable, (err, results) => {
     if (err) {
-      console.error('Erro ao criar a tabela de times:', err);
+      console.error("Erro ao criar a tabela de times:", err);
     } else {
-      console.log('Tabela de times criada ou já existe.');
+      console.log("Tabela de times criada ou já existe.");
     }
   });
 
   pool.query(createJogadoresTable, (err, results) => {
     if (err) {
-      console.error('Erro ao criar a tabela de jogadores:', err);
+      console.error("Erro ao criar a tabela de jogadores:", err);
     } else {
-      console.log('Tabela de jogadores criada ou já existe.');
+      console.log("Tabela de jogadores criada ou já existe.");
     }
   });
 
   pool.query(createPartidasTable, (err, results) => {
     if (err) {
-      console.error('Erro ao criar a tabela de partidas:', err);
+      console.error("Erro ao criar a tabela de partidas:", err);
     } else {
-      console.log('Tabela de partidas criada ou já existe.');
+      console.log("Tabela de partidas criada ou já existe.");
     }
   });
 
   pool.query(createGolsTable, (err, results) => {
     if (err) {
-      console.error('Erro ao criar a tabela de gols:', err);
+      console.error("Erro ao criar a tabela de gols:", err);
     } else {
-      console.log('Tabela de gols criada ou já existe.');
+      console.log("Tabela de gols criada ou já existe.");
     }
   });
 
   const a = `
-    INSERT INTO usuarios (email, nome, senha, cpf, administrador) VALUES
-    ('adm_time1@example.com', 'Admin Time 1', 'senha123', '12345678900', 1),
-    ('adm_time2@example.com', 'Admin Time 2', 'senha123', '12345678901', 1),
-    ('adm_time3@example.com', 'Admin Time 3', 'senha123', '12345678902', 1),
-    ('adm_time4@example.com', 'Admin Time 4', 'senha123', '12345678903', 1);
+  INSERT INTO usuarios (email, nome, senha, cpf, administrador) VALUES
+  ('adm_time1@example.com', 'Admin Time 1', 'senha123', '12345678900', 1);
   
-    INSERT INTO times (nome, sigla, local, capitao_id, cor_principal, cor_secundaria) VALUES
-    ('Time 1', 'T1', 'Estádio 1', 1, 'Azul', 'Branco'),
-    ('Time 2', 'T2', 'Estádio 2', 2, 'Verde', 'Amarelo'),
-    ('Time 3', 'T3', 'Estádio 3', 3, 'Vermelho', 'Preto'),
-    ('Time 4', 'T4', 'Estádio 4', 4, 'Laranja', 'Cinza');
+  INSERT INTO usuarios (email, nome, senha, cpf, administrador) VALUES
+  ('adm_time2@example.com', 'Admin Time 2', 'senha123', '12345678901', 1);
   
-    INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
-    ('Goleiro Time 1', 1, 1, 30, 'Goleiro', 1),
-    ('Zagueiro Time 1', 2, 1, 28, 'Zagueiro', NULL),
-    ('Zagueiro Time 1', 3, 1, 26, 'Zagueiro', NULL),
-    ('Atacante Time 1', 9, 1, 24, 'Atacante', NULL),
-    ('Atacante Time 1', 10, 1, 23, 'Atacante', NULL),
-    ('Goleiro Time 2', 1, 2, 32, 'Goleiro', 2),
-    ('Zagueiro Time 2', 2, 2, 27, 'Zagueiro', NULL),
-    ('Zagueiro Time 2', 3, 2, 25, 'Zagueiro', NULL),
-    ('Atacante Time 2', 9, 2, 22, 'Atacante', NULL),
-    ('Atacante Time 2', 10, 2, 21, 'Atacante', NULL),
-    ('Goleiro Time 3', 1, 3, 29, 'Goleiro', 3),
-    ('Zagueiro Time 3', 2, 3, 28, 'Zagueiro', NULL),
-    ('Zagueiro Time 3', 3, 3, 27, 'Zagueiro', NULL),
-    ('Atacante Time 3', 9, 3, 25, 'Atacante', NULL),
-    ('Atacante Time 3', 10, 3, 23, 'Atacante', NULL),
-    ('Goleiro Time 4', 1, 4, 31, 'Goleiro', 4),
-    ('Zagueiro Time 4', 2, 4, 26, 'Zagueiro', NULL),
-    ('Zagueiro Time 4', 3, 4, 24, 'Zagueiro', NULL),
-    ('Atacante Time 4', 9, 4, 27, 'Atacante', NULL),
-    ('Atacante Time 4', 10, 4, 22, 'Atacante', NULL);
+  INSERT INTO usuarios (email, nome, senha, cpf, administrador) VALUES
+  ('adm_time3@example.com', 'Admin Time 3', 'senha123', '12345678902', 1);
   
-    INSERT INTO partidas (data, time_1, time_2, finalizada) VALUES
-    ('2024-11-15 16:00:00', 1, 2, 0),
-    ('2024-11-16 18:00:00', 3, 4, 0);
-  
-    INSERT INTO gols (partida_id, minuto, jogador_id, time_id) VALUES
-    (1, 15, 1, 1),
-    (1, 30, 4, 1),
-    (1, 40, 2, 2),
-    (1, 60, 6, 2),
-    (2, 10, 1, 3),
-    (2, 35, 9, 3),
-    (2, 50, 8, 4),
-    (2, 70, 10, 4);
-  `;
-  
-  pool.query(a, (err, results) => {
-    if (err) {
-      console.error('Erro ao inserir dados:', err);
-    } else {
-      console.log('Dados inseridos com sucesso.');
-    }
+  INSERT INTO usuarios (email, nome, senha, cpf, administrador) VALUES
+  ('adm_time4@example.com', 'Admin Time 4', 'senha123', '12345678903', 1);
+
+  INSERT INTO times (nome, sigla, local, capitao_id, cor_principal, cor_secundaria) VALUES
+  ('Time 1', 'T1', 'Estádio 1', 1, 'Azul', 'Branco');
+
+  INSERT INTO times (nome, sigla, local, capitao_id, cor_principal, cor_secundaria) VALUES
+  ('Time 2', 'T2', 'Estádio 2', 2, 'Verde', 'Amarelo');
+
+  INSERT INTO times (nome, sigla, local, capitao_id, cor_principal, cor_secundaria) VALUES
+  ('Time 3', 'T3', 'Estádio 3', 3, 'Vermelho', 'Preto');
+
+  INSERT INTO times (nome, sigla, local, capitao_id, cor_principal, cor_secundaria) VALUES
+  ('Time 4', 'T4', 'Estádio 4', 4, 'Laranja', 'Cinza');
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Goleiro Time 1', 1, 1, 30, 'Goleiro', 1);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Zagueiro Time 1', 2, 1, 28, 'Zagueiro', NULL);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Zagueiro Time 1', 3, 1, 26, 'Zagueiro', NULL);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Atacante Time 1', 9, 1, 24, 'Atacante', NULL);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Atacante Time 1', 10, 1, 23, 'Atacante', NULL);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Goleiro Time 2', 1, 2, 32, 'Goleiro', 2);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Zagueiro Time 2', 2, 2, 27, 'Zagueiro', NULL);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Zagueiro Time 2', 3, 2, 25, 'Zagueiro', NULL);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Atacante Time 2', 9, 2, 22, 'Atacante', NULL);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Atacante Time 2', 10, 2, 21, 'Atacante', NULL);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Goleiro Time 3', 1, 3, 29, 'Goleiro', 3);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Zagueiro Time 3', 2, 3, 28, 'Zagueiro', NULL);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Zagueiro Time 3', 3, 3, 27, 'Zagueiro', NULL);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Atacante Time 3', 9, 3, 25, 'Atacante', NULL);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Atacante Time 3', 10, 3, 23, 'Atacante', NULL);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Goleiro Time 4', 1, 4, 31, 'Goleiro', 4);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Zagueiro Time 4', 2, 4, 26, 'Zagueiro', NULL);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Zagueiro Time 4', 3, 4, 24, 'Zagueiro', NULL);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Atacante Time 4', 9, 4, 27, 'Atacante', NULL);
+
+  INSERT INTO jogadores (nome_completo, numero, time_id, idade, posicao, usuario_id) VALUES
+  ('Atacante Time 4', 10, 4, 22, 'Atacante', NULL);
+
+  INSERT INTO partidas (data, time_1, time_2, finalizada) VALUES
+  ('2024-11-15 16:00:00', 1, 2, 0);
+
+  INSERT INTO partidas (data, time_1, time_2, finalizada) VALUES
+  ('2024-11-16 18:00:00', 3, 4, 0);
+
+  INSERT INTO gols (partida_id, minuto, jogador_id, time_id) VALUES
+  (1, 15, 1, 1);
+
+  INSERT INTO gols (partida_id, minuto, jogador_id, time_id) VALUES
+  (1, 30, 4, 1);
+
+  INSERT INTO gols (partida_id, minuto, jogador_id, time_id) VALUES
+  (1, 40, 2, 2);
+
+  INSERT INTO gols (partida_id, minuto, jogador_id, time_id) VALUES
+  (1, 60, 6, 2);
+
+  INSERT INTO gols (partida_id, minuto, jogador_id, time_id) VALUES
+  (2, 10, 1, 3);
+
+  INSERT INTO gols (partida_id, minuto, jogador_id, time_id) VALUES
+  (2, 35, 9, 3);
+
+  INSERT INTO gols (partida_id, minuto, jogador_id, time_id) VALUES
+  (2, 50, 8, 4);
+
+  INSERT INTO gols (partida_id, minuto, jogador_id, time_id) VALUES
+  (2, 70, 10, 4);
+`;
+
+  const queries = a
+    .split(";")
+    .map((query) => query.trim())
+    .filter((query) => query);
+
+  queries.forEach((query) => {
+    pool.query(query, (err, results) => {
+      if (err) {
+        console.error("Erro ao inserir dados:", err);
+      } else {
+        console.log("Dados inseridos com sucesso.");
+      }
+    });
   });
 };
 
 function repopulateDB(req, res) {
-  console.log('Repopulando o banco de dados...');
+  console.log("Repopulando o banco de dados...");
   createTables();
-  console.log('Repopulação concluída.');
-  res.json({ message: 'Repopulação concluída.' });
+  console.log("Repopulação concluída.");
+  res.json({ message: "Repopulação concluída." });
 }
 
 module.exports = repopulateDB;
